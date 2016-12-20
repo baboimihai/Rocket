1441a212ccbe0ebfb1626e1361fd17fbad1bd818
Oana Rosu <rosu.oanamaria@outlook.com>
2016-12-20 02:26:30 +0200

    Added functionality for saving a user's answers and some aiml files

--- KB/KnowledgeBase/AimlCommunication.py
+++ KB/KnowledgeBase/AimlCommunication.py
@@ -5,21 +5,33 @@ import glob
 context = dict()
 
 class Parser:
+    filesList = ["alfa_RocketBot", "std-brain", "std-dictionary", "std-geography", "std-inventions",
+                 "std-knowledge", "std-personality", "std-pickup", "std-sextalk", "std-sports"]
     rootList = []
     currentUser = ""
     userRoot = ""
+    yesNoRoot = ""
     def __init__(self, username):
-        #IMPORTANT!! folositi "r" inaintea stringului, poate provoca erori altfel ?
-        tree = ET.parse("aiml/alfa_RocketBot.aiml")
-        self.rootList.append(tree.getroot())
-
+        self.processAimlFiles()
         self.currentUser = username
+
+        tree = ET.parse("aiml/yes-no.aiml")
+        self.yesNoRoot = tree.getroot()
+
         if self.searchForFile(username) is True:
             userTree = ET.parse("aiml/user_definitions/" + username.lower() + '.aiml')
             self.userRoot = userTree.getroot()
+        else:
+            self.createAimlFileForUser(username.lower())
 
         context["it"] = ""
         context["topic"] = ""
+
+    def processAimlFiles(self):
+        # IMPORTANT!! folositi "r" inaintea stringului, poate provoca erori altfel ?
+        for file in self.filesList:
+            tree = ET.parse("aiml/" + file + ".aiml")
+            self.rootList.append(tree.getroot())
 
     def selectRandomAnswer(self, rand):
         randList = list()
@@ -43,10 +55,12 @@ class Parser:
         return answer
 
     def findPatternInCurrentRoot(self, pattern, currentRoot):
+        #TODO: process <srai>, <think>, <that>, <get name>
         for categ in currentRoot:
             if (categ.tag == "category"):
                 for pat in categ:
-                    if pat.tag == "pattern" and pat.text.lower() == pattern.lower():
+                    #TODO: improve search by using reg expressions
+                    if pat.tag == "pattern" and (pat.text is not None and pat.text.lower() == pattern.lower()):
                         for tem in categ:
                             if (tem.tag == "template"):
                                 return self.processTemplate(tem)
@@ -61,7 +75,8 @@ class Parser:
         else:
             return result
 
-    #TODO: creaza noi fisiere aiml pentru fiecare nou utilizator
+    def processAnswer(self, text):
+        return self.findPatternInCurrentRoot(text, self.yesNoRoot)
 
     # cauta arhiva pentru un user
     def searchForFile(self, username):
@@ -71,6 +86,25 @@ class Parser:
             if filename == file.lower():
                 print("found")
                 return True
+
+    def createAimlFileForUser(self, username):
+        fo = open("aiml/user_definitions/"+username+".aiml", "w")
+        fo.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<aiml version=\"1.0\">\n")
+        fo.write("</aiml>")
+        fo.close()
+
+    def saveAnswerInUserFile(self, answer, question):
+        fo = open("aiml/user_definitions/" + self.currentUser + ".aiml", "r+")
+        fo.seek(0, 2)
+        size = fo.tell()
+        fo.truncate(size - 7)  # truncate </aiml>
+        fo.seek(0, 2)
+        fo.write("<category><pattern>" + question + "</pattern>\n<template>" + answer + "</template>\n</category>\n");
+        fo.write("</aiml>")
+        fo.close()
+
+        self.userRoot = ET.parse("aiml/user_definitions/" + self.currentUser.lower() + '.aiml').getroot()
+
 
 #exista in ductionar "context" care contiune cateva date cateva date despre discutia curenta. Irelevant momentan
 #print ("Topic: "+AimlCommunication.context["topic"]+"\n")
--- KB/testImport.py
+++ KB/testImport.py
@@ -9,11 +9,18 @@ def talkWithTheChatbot():
 
     while True:
         pattern = input("Me> ")
+        #TODO: goodbye synonims
         result = parser.findPattern(pattern)
         if result is not None:
             print("Rocket> " + result)
         else:
             print("Rocket> " + "I can't help you. Do you want to give me the answer?")
-            #TODO: tratare cazuri
+            answer = input("Me> ")
+            if parser.processAnswer(answer) == "yes":
+                print("Rocket> " + "Give me the answer.")
+                answer = input("Me> ")
+                parser.saveAnswerInUserFile(answer, pattern)
+            else:
+                print("Rocket> Ok")
 
 talkWithTheChatbot()
