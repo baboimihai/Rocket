dcaae1965ccf6613a883cff920e8cb37061b2a3f
DumeaClaudia <DumeaClaudia@users.noreply.github.com>
2017-01-17 11:59:57 +0200

    Update wiki_search.py

--- PostProcessing/wiki_search.py
+++ PostProcessing/wiki_search.py
@@ -1,16 +1,25 @@
 
 import wikipedia
 from nltk.tokenize import sent_tokenize
-from Brain.WikiSearch import highestMatch
 
-def first_sentence(sentence):
-    
-    searchCriteria = wikipedia.search(sentence)
-    word = highestMatch(searchCriteria, sentence)
 
-    information = str(wikipedia.summary(word).encode('utf-8'))
-    sent_tokenize_list = sent_tokenize(information)
-    return sent_tokenize_list[0]
+def first_sentence(words):
+    try:
+        information = wikipedia.summary(" ".join(words)).encode('utf-8', 'ignore').decode('utf-8', 'ignore')
+        sent_tokenize_list = sent_tokenize(information)
+        return sent_tokenize_list[0]
+    except:
+        return None
 
-print(first_sentence("onion"))
+
+def get_response(subjects):
+    for i in range(len(subjects), 1, -1):
+        a = first_sentence(subjects[:i])
+        if a is not None:
+            return [a, True]
+    return ["", False]
+
+
+print(first_sentence(["President", "United States of America"]))
+
 
