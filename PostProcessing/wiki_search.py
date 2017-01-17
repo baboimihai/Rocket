e556cdcf48d5922945caff6b086a9dc866a36228
DumeaClaudia <DumeaClaudia@users.noreply.github.com>
2017-01-17 10:42:06 +0200

    Update wiki_search.py

--- PostProcessing/wiki_search.py
+++ PostProcessing/wiki_search.py
@@ -1,11 +1,16 @@
 
 import wikipedia
 from nltk.tokenize import sent_tokenize
+from Brain.WikiSearch import highestMatch
 
+def first_sentence(sentence):
+    
+    searchCriteria = wikipedia.search(sentence)
+    word = highestMatch(searchCriteria, sentence)
 
-def first_sentence(word):
     information = str(wikipedia.summary(word).encode('utf-8'))
     sent_tokenize_list = sent_tokenize(information)
     return sent_tokenize_list[0]
 
 print(first_sentence("onion"))
+
